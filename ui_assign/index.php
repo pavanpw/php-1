12b8b9724acff86f108e8a5c2ed2bbbbf6b22fe9
vedavaty9293 <vbadaravada9293@gmail.com>
2020-11-10 14:44:55 +1100

    adding ui assignment

---
+++ ui_assign/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE htmls>
+<html lang="en">
+<head>
+<meta charset ="UTF -8">
+<meta name = "viewport" content="width=device-width,intial-scale=1.0">
+<title>Document</title>
+</head>
+<body>
+<?php
+echo "Hello World";
+?>
+</body>
+</html>
